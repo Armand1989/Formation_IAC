1166b338ae5bf25d1e248530530254a0dd344d2d
Armand BUISSON <abuisson@d2-si.eu>
2018-09-03 14:51:26 +0200

    subnet ok

--- CoreInfra/main.tf
+++ CoreInfra/main.tf
@@ -5,3 +5,12 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = "172.23.0.0/16"
 }
+
+resource "aws_subnet" "main" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "172.23.1.0/24"
+
+  tags {
+    Name = "Main"
+  }
+}
