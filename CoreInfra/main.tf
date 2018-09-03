f7375c2163f0cfcc9656f0692b856b973d976338
Armand BUISSON <abuisson@d2-si.eu>
2018-09-03 15:01:50 +0200

    Route table association ok

--- CoreInfra/main.tf
+++ CoreInfra/main.tf
@@ -35,3 +35,8 @@ resource "aws_route_table" "r" {
     Name = "main"
   }
 }
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = "${aws_subnet.main.id}"
+  route_table_id = "${aws_route_table.r.id}"
+}
